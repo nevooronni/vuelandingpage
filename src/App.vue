4aa6e0208ce895bf2b9a855ff7066fa3631e2572
Neville Oronni <nevooronni@gmail.com>
2018-03-14 02:34:59 +0300

    added first template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
+    <div class="nav has-shadow">
+      <div class="container">
+        <div class="navbar-start">
+          <a class="nav-item">MyCompany</a>
+        </div>
+
+        <span class="nav-toggel">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="navbar-end nav-menu">
+        <div class="nav-item">
+          <router-link to="/" class="nav item r-item">Home</router-link>
+          <router-link to="faq" class="nav item r-item">Features</router-link>
+          <router-link to="faq" class="nav item r-item">About</router-link>
+          <router-link to="faq" class="nav item r-item">FAQ</router-link>
+        </div>
+          <div class="nav-item">
+            <p class="control">
+              <a class="button is-primary is-outlined">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+                <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -15,11 +48,4 @@ export default {
 @import '../node_modules/bulma/bulma.sass'
 @import 'mq'
 
-#app 
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center;
-  color: #2c3e50
-  margin-top: 60px
 </style>
